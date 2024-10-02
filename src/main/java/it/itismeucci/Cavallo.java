1f96cf3f419f36e9d324d4c4fe2e4d92456d6020
Mattia Cugusi <m.cugusi287@gmail.com>
2024-10-02 10:14:12 +0200

    corsa cavalli completata

---
+++ src/main/java/it/itismeucci/Cavallo.java
@@ -0,0 +1,43 @@
+package it.itismeucci;
+
+public class Cavallo extends Thread {
+
+    private int l;
+    private int identificativo;
+
+    public Cavallo(int lunghezza, int id){
+        l = lunghezza;
+        identificativo = id;
+    }
+
+    private int attesa;
+
+
+    public void run() {
+        for (int i=0; i<l; i++){
+            try {
+                Thread.sleep((int)(Math.random() * 100));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        fine();
+        
+    }
+
+
+    private synchronized void fine(){
+
+     Main.classifica.add(this);
+
+    }
+
+    @Override
+
+    public String toString(){
+        return "Cavallo n: " + identificativo;
+    }
+
+}
--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,12 +1,48 @@
 package it.itismeucci;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+
+  public static List<Cavallo> classifica = new ArrayList<>();
+    public static void main(String[] args) throws InterruptedException {
+
+      
 
         int l=15;
-        MioThread t1 = new MioThread(l);
-        MioThread t2 = new MioThread(l);
-        Thread t3 = new Thread(t1);
-        t3.start();
+        Cavallo c1 = new Cavallo(l, 1);
+        c1.start();
+        Cavallo c2 = new Cavallo(l, 2);
+        c2.start();
+        Cavallo c3 = new Cavallo(l, 3);
+        c3.start();
+        Cavallo c4 = new Cavallo(l, 4);
+        c4.start();
+        Cavallo c5 = new Cavallo(l, 5);
+        c5.start();
+        Cavallo c6 = new Cavallo(l, 6);
+        c6.start();
+        Cavallo c7 = new Cavallo(l, 7);
+        c7.start();
+        Cavallo c8 = new Cavallo(l, 8);
+        c8.start();
+        Cavallo c9 = new Cavallo(l, 9);
+        c9.start();
+        Cavallo c10 = new Cavallo(l, 10);
+        c10.start();
+
+        c1.join();
+        c2.join();
+        c3.join();
+        c4.join();
+        c5.join();
+        c6.join();
+        c7.join();
+        c8.join();
+        c9.join();
+        c10.join();
+
+        System.out.println(classifica);
   }
 }
